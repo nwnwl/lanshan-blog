3ef4aae08db4d9a024e00591d0fac3497b94471b
nwnwl <wn17782008094@outlook.com>
2026-07-28 15:48:51 +0800

    refactor(frontend): 简化 About 区块布局层级，调整信息栏相对定位

--- apps/frontend/src/app/(PC)/content/About/About.tsx
+++ apps/frontend/src/app/(PC)/content/About/About.tsx
@@ -5,70 +5,64 @@ import styles from './About.module.css';
 export const PC_AboutSection = () => {
   return (
     <section id="about" className="min-h-screen w-full">
-      <div className="relative">
-        {/* 上侧 */}
-        <div className="w-full min-h-0 flex ml-60 my-16">
-          {/* 左侧 */}
-          <div
-            className={`header_about flex flex-col justify-between
+      <div className="w-full min-h-0 flex ml-60 mt-16">
+        {/* 左侧 */}
+        <div
+          className={`header_about flex flex-col justify-between
           ${styles.aboutHeader}`}
-          >
-            {/* header部分 */}
-            <div className="flex flex-col justify-center">
-              <div className="flex items-center">
-                <div
-                  className="flex 
+        >
+          {/* header部分 */}
+          <div className="flex flex-col justify-center">
+            <div className="flex items-center">
+              <div
+                className="flex 
               md:h-5 md:w-18
               h-4 w-10  
                bg-[#D9D9D9] justify-end items-center pr-1"
-                >
-                  <Icon
-                    name="arrow"
-                    className="SectionTitle_arrow__qXHl 
+              >
+                <Icon
+                  name="arrow"
+                  className="SectionTitle_arrow__qXHl 
                 md:w-4 md:h-4
                 w-3 h-3
                 "
-                  />
-                </div>
-                <span className="lg:text-2xl text-xs font-medium pl-2">ABOUT US</span>
+                />
               </div>
-              <div className="font-bold lg:text-4xl md:text-2xl text-xl tracking-tight">
-                关于我们
-              </div>
+              <span className="lg:text-2xl text-xs font-medium pl-2">ABOUT US</span>
             </div>
-
-            {/* 左侧装饰 */}
-            <div className="p-4 bg-[#D9D9D9] w-fit">
-              <Icon
-                name="gameplay"
-                className="__05-Gameplay_icon__Yiqki md:w-15 md:h-15 h-10 w-10"
-              />
-            </div>
+            <div className="font-bold lg:text-4xl md:text-2xl text-xl tracking-tight">关于我们</div>
           </div>
 
-          {/* 右侧图片 */}
-          <div className="pl-32 flex">
+          {/* 左侧装饰 */}
+          <div className="p-4 bg-[#D9D9D9] w-fit">
+            <Icon name="gameplay" className="__05-Gameplay_icon__Yiqki md:w-15 md:h-15 h-10 w-10" />
+          </div>
+        </div>
+
+        {/* 右侧图片 */}
+        <div className="pl-32 flex">
+          <div className="relative">
             <img
               src="/picture/studio-1.png"
               alt="studio picture"
               className="w-[1305.420px] h-[767.891px]"
             />
-            {/* 蓝色装饰 */}
-            <div className="bg-[#00D4FF] w-[268.109px] py-4 pl-6">
-              <div className="bg-white h-full w-6 relative">
-                <span className="absolute top-2 [writing-mode:vertical-rl] md:text-5xl text-2xl font-semibold tracking-wider">
-                  OVERVIEW
-                </span>
+
+            {/* 下侧信息栏 */}
+            <div className="absolute w-[811.375px] ">
+              <div className="text-[42.3333px] font-semibold">探索工作室</div>
+              <div className="text-[26.4583px] font-medium">
+                蓝山工作室是由教育信息化办公室/信息中心指导的，从事互联网产品、教育信息化产品开发运营和服务全体师生的校级学生组织。
               </div>
             </div>
           </div>
-        </div>
-
-        {/* 下侧信息栏 */}
-        <div className="absolute -bottom-40 left-156 w-[811.375px] ">
-          <div className="text-[42.3333px] font-semibold">探索工作室</div>
-          <div className="text-[26.4583px] font-medium">
-            蓝山工作室是由教育信息化办公室/信息中心指导的，从事互联网产品、教育信息化产品开发运营和服务全体师生的校级学生组织。
+          {/* 蓝色装饰 */}
+          <div className="bg-[#00D4FF] w-[268.109px] py-4 pl-6">
+            <div className="bg-white h-full w-6 relative">
+              <span className="absolute top-2 [writing-mode:vertical-rl] md:text-5xl text-2xl font-semibold tracking-wider">
+                OVERVIEW
+              </span>
+            </div>
           </div>
         </div>
       </div>
